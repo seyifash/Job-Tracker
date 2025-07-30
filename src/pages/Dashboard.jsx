eed6119a7e70d64136b9f3939f09a283d588a5ee
oluwaseyi <99446401+seyifash@users.noreply.github.com>
2025-07-30 16:31:44 +0100

    Update Dashboard.jsx
    upodate

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import DashboardLayout from '../components/DashboardLayout';
-import JobForm from '../components/Jobform';
+import JobForm from '../components/JobForm';
 
 const Dashboard = () => {
   const [jobs, setJobs] = useState(() => {
